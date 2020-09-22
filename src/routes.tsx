e76c66d0b5f8c59da5de506dbfb6102c093d95da
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-09-21 22:05:35 -0300

    wip

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import Header from './components/Header';
 import Main from './pages/Main';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Route exact path="/" component={Main} />
+      <Route exact path="/:username" component={Main} />
+      <Route exact path="/:username/:repo" component={Main} />
     </BrowserRouter>
   );
 };
